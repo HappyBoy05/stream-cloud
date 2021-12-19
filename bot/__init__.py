3868186e3dd0c837fc9512e7d4679fb90ef84615
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-19 17:08:52 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -40,7 +40,7 @@ async def download(event):
                     participant = event.sender_id
                     ))
             except errors.UserNotParticipantError:
-                await event.reply(f"First join to our official channel to access the bot or get the newest news about the bot\n\n@{Config.CHANNEL_USERNAME}\n\nAfter that /start the bot aging.")
+                await event.reply(f"First join Our Channel to access the bot\n\nJoin @{Config.CHANNEL_USERNAME}\n\nAfter that /start the bot aging.")
                 return
         
         if event.file :
@@ -55,7 +55,7 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
+            await event.reply(f"Download Link: \n\n📎 : {Config.DOMAIN}/{id}\n\n Telegram Stored Link: {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -76,16 +76,16 @@ async def download(event):
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
                         bot_url = f"t.me/{username_bot}?start={id_hex}"
-                        forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
+                        forward_reply = await forward.reply(f"Download Link: {Config.DOMAIN}/{id_name}\n\nTelegram Stored Link: {bot_url}",link_preview=False)
                         await asyncio.sleep(12)
-                        await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
+                        await forward_reply.edit(f"Download Link: {Config.DOMAIN}/{id_name}\n\nTelegram Stored Link: {bot_url}")
                         await asyncio.sleep(10)
                         await forward.delete()
-                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
+                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n Telegram Stored Link: {bot_url}")
                 return
         
         if pv:
-            await event.reply("Send any file to get a link to download it")
+            await event.reply("Send Any File To Get a Direct Download Link And Telegram Stored Link")
         
 
     elif event.is_channel:
